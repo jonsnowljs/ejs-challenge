8786a3a0eb9072d67921196bbe57633dbdc1a151
jonsnowljs <jonsnowljs@gmail.com>
2021-09-12 23:51:50 -0600

    add lodash and other feature

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,7 +20,6 @@ app.use(express.static("public"));
 
 app.get("/", function(req,res) {
   res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
-  console.log(posts);
 });
 
 app.post("/",function(req,res){
@@ -27,10 +27,6 @@ app.post("/",function(req,res){
   posts.push(post);
   res.redirect("/");
 
-});
-
-app.get("/:postsTitle", function(req,res) {
-  console.log(req.params.postsTitle);
 });
 
 app.get("/contact", function(req,res) {
@@ -43,6 +39,18 @@ app.get("/about", function(req,res) {
 
 app.get("/compose", function(req,res) {
   res.render("compose", {aboutContent: aboutContent});
+});
+
+app.get("/posts/:postsTitle", function(req,res) {
+  const titleName = req.params.postsTitle;
+  posts.forEach(function(post){
+    if (lodash.lowerCase(titleName) === lodash.lowerCase(post.title)) {
+      console.log("success");
+    } else {
+      console.log("not a match");
+    }
+  })
+
 });
 
 app.listen(3000, function() {
